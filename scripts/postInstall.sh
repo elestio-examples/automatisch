b018cf17ce788dc7d111780aff181d1b47fce401
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-01-29 17:04:28 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -35,7 +35,7 @@ login=$(curl http://${target}/graphql \
   curl http://${target}/graphql \
   -H 'accept: */*' \
   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H authorization: ${token} \
+  -H 'authorization: '${token} \
   -H 'content-type: application/json' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
   --data-raw '{"operationName":"UpdateUser","variables":{"input":{"email":'${ADMIN_EMAIL}'}},"query":"mutation UpdateUser($input: UpdateUserInput) {\n  updateUser(input: $input) {\n    id\n    email\n    updatedAt\n    __typename\n  }\n}\n"}' \
@@ -47,7 +47,7 @@ login=$(curl http://${target}/graphql \
   curl http://${target}/graphql \
   -H 'accept: */*' \
   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
-  -H authorization: ${token} \
+  -H 'authorization: '${token} \
   -H 'content-type: application/json' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
   --data-raw '{"operationName":"UpdateUser","variables":{"input":{"password":'"${ADMIN_PASSWORD}"'}},"query":"mutation UpdateUser($input: UpdateUserInput) {\n  updateUser(input: $input) {\n    id\n    email\n    updatedAt\n    __typename\n  }\n}\n"}' \
